e96c6c00a1317135ecc83ae7bc3126034e9918cb
IlyaNikitsin <JamsN87@gmail.com>
2023-02-03 09:34:18 +0300

    add perevorot mas

--- Seminar/Program.cs
+++ Seminar/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+void InputArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 11);
+    }
+}
+
+void ReplaceElement(int[] array)
+{
+    for(int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - i - 1];
+        array[array.Length - i - 1] = temp;
+    }
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный масссив: [{string.Join(", ", array)}]");
+ReplaceElement(array);
+Console.WriteLine($"Конечный масссив: [{string.Join(", ", array)}]");
