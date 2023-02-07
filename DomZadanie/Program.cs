82c237d897cbd84b971e9f6248232f3982ae82fd
IlyaNikitsin <JamsN87@gmail.com>
2023-02-07 11:47:11 +0300

    fin task 41, 43

--- DomZadanie/Program.cs
+++ DomZadanie/Program.cs
@@ -1,4 +1,6 @@
-﻿// Task 41 
+﻿Console.Clear();
+
+// Task 41 
 // Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 //0, 7, 8, -2, -2 -> 2     1, -7, 567, 89, 223-> 3
 /*
@@ -20,4 +22,21 @@ Console.WriteLine($"Количест чисел больше 0 = {r}");
 // Task 43
 // Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+/*
+Console.WriteLine("Введите b1 : ");
+    double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k1 : ");
+    double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2 : ");
+    double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2 : ");
+    double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+Console.WriteLine($" точка пересечения двух прямых : ({Math.Round(x, 2)} , {Math.Round(y , 2)})");
+*/
+
+// Task Рекурсия
+// Дана строка, состоящая из N попарно различных символов. Требуется вывести все перестановки символов данной строки.
 
