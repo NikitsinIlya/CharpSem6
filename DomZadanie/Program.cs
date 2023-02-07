f66a4a648c41f713271a81819df0c97a29ba6f57
IlyaNikitsin <JamsN87@gmail.com>
2023-02-07 08:17:32 +0300

    add task41

--- DomZadanie/Program.cs
+++ DomZadanie/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 41 
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2     1, -7, 567, 89, 223-> 3
+/*
+Console.WriteLine("Введите количество чисел: ");
+    int M = Convert.ToInt32(Console.ReadLine()), s = 0, n = 0, r = 0;
+    int[] array = new int[M];
+for(int i = 0; i < M; i++)
+{
+    Console.WriteLine($"Введите число {s + 1}: ");
+    n = Convert.ToInt32(Console.ReadLine());
+    array[s] = n;
+    s++;
+        if (n > 0 ) r++;
+}
+Console.WriteLine($"{string.Join(" ,  ", array)}");
+Console.WriteLine($"Количест чисел больше 0 = {r}");
+*/
+
+// Task 43
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
